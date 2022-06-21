863d86473610b0105829c60742418350033c7db2
rafaaaax <93734330+rafaaaax@users.noreply.github.com>
2022-06-21 17:19:06 -0400

    probando

--- Taller/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rafae/Documents/GitHub/ED22-01-Fredes-Munizaga/Taller
-# Build directory: C:/Users/rafae/Documents/GitHub/ED22-01-Fredes-Munizaga/Taller/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
